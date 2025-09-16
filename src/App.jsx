1635b429515695a2f3eafe8ed9603763e4923672
Hassan ur Rahman P.M <pmhassanurrahman@gmail.com>
2025-09-16 16:28:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,48 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+
 import Dashboard from "./pages/Dashboard";
 import Habit from "./pages/Habit";
 import Progress from "./pages/Progress";
 import Calender from "./pages/Calender";
+import Settings from "./pages/Settings";
 import Layout from "./Component/Layout";
 import Login from "./Login/Login";
-import { useEffect, useState } from "react";
-import Settings from "./pages/Settings";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase";
-
 
 const App = () => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true); // new loading state
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false); // auth check finished
     });
     return () => unsubscribe();
   }, []);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        Loading...
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-screen">
       <Routes>
         <Route
           path="/login"
-          element={<Login onLogin={(email) => setUser(email)} />}
+          element={
+            !user ? (
+              <Login onLogin={(email) => setUser(email)} />
+            ) : (
+              <Navigate to="/dashboard" />
+            )
+          }
         />
 
         <Route path="/" element={user ? <Layout /> : <Navigate to="/login" />}>
@@ -35,7 +51,6 @@ const App = () => {
           <Route path="progress" element={<Progress />} />
           <Route path="calender" element={<Calender />} />
           <Route path="settings" element={<Settings />} />
-
           <Route index element={<Navigate to="dashboard" />} />
         </Route>
 
